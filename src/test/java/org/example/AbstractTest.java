bcf25b3381bc8ee1d12ac87511d737797373f032
maksimvaulin <quest.skamsk@mail.com>
2021-12-21 21:10:47 +0500

    Abstract

--- src/test/java/org/example/AbstractTest.java
+++ src/test/java/org/example/AbstractTest.java
@@ -1,17 +1,18 @@
 package org.example;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
-public class deliveryTest {
+public class AbstractTest {
+
     static WebDriver webDriver;
 
     @BeforeAll
@@ -31,19 +32,4 @@ public class deliveryTest {
         Assertions.assertDoesNotThrow( ()-> webDriver.navigate().to("https://meatfish.cafe"), "Страница не доступна");
     }
 
-    @Test
-    void deliveryTest(){
-        WebElement delivery = webDriver.findElement(By.xpath("//div/div/header/nav[1]/div[2]/div/ul/li[3]/a"));
-        delivery.click();
-
-        Actions actions = new Actions(webDriver);
-        actions.moveToElement(webDriver.findElement(By.xpath("//div/div/main/div[3]/h1"))).build().perform();
-
-    }
-
-    @AfterAll
-    public static void exit() {
-        if (webDriver !=null)webDriver.quit();
-    }
 }
-
